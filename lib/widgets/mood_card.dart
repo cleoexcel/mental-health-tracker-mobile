b36bbf14127d0c109734b2b3367b2919202babe6
cleoexcel <cleoxclln@gmail.com>
2024-11-06 14:21:52 +0700

    Tutorial 7

--- lib/widgets/mood_card.dart
+++ lib/widgets/mood_card.dart
@@ -9,44 +9,30 @@ class ItemHomepage {
 }
 
 class ItemCard extends StatelessWidget {
-  // Display the card with an icon and name.
-
   final ItemHomepage item;
-
   const ItemCard(this.item, {super.key});
-
   @override
   Widget build(BuildContext context) {
     return Material(
-      // Specify the background color of the application theme.
       color: Theme.of(context).colorScheme.secondary,
-      // Round the card border.
       borderRadius: BorderRadius.circular(12),
-
       child: InkWell(
-        // Touch-responsive area
         onTap: () {
-          // Show SnackBar when clicked
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("You pressed the ${item.name} button!")));
-
-          // Navigate to the appropriate route (depending on the button type)
-          if (item.name == "Add Mood") {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const MoodEntryFormPage()),
-            );
-          }
+            ScaffoldMessenger.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(SnackBar(
+                    content: Text("Kamu telah menekan tombol ${item.name}!")));
+            if (item.name == "Tambah Mood") {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MoodEntryFormPage()),
+                );
+            }
         },
-
-        // Container to store the Icon and Text
         child: Container(
           padding: const EdgeInsets.all(8),
           child: Center(
             child: Column(
-              // Place the Icon and Text in the center of the card.
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
